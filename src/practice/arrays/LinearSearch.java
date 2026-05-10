304a7df6ac31d199e3cda859aa127e8fa74f4da7
AvinashPatel26 <patelavinash2693@gmail.com>
2026-05-10 15:28:11 +0530

    practice started

--- src/practice/arrays/LinearSearch.java
+++ src/practice/arrays/LinearSearch.java
@@ -1,4 +1,19 @@
 package practice.arrays;
 
 public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {18, 12, 9, 14, 77, 50};
+
+        boolean result = linearSearch(arr, 14);
+        System.out.println(result);
+    }
+
+    private static boolean linearSearch(int[] arr, int target) {
+        for (int element : arr) {
+            if (element == target) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
